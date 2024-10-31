38c2f331c214ea2bb1e0a88ad811df17c70e238d
Sierra Erb <Serb2341@conestogac.on.ca>
2024-10-31 16:45:08 -0400

    Added settings pages and logic for settings page

---
+++ Group_3/FrontEnd/app/src/main/java/com/example/calamitycall/NotificationTypeActivity.java
@@ -0,0 +1,37 @@
+package com.example.calamitycall;
+
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationTypeActivity extends AppCompatActivity {
+
+    private Switch notificationSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_notification_on_page);
+
+        // Initialize the switch
+        notificationSwitch = findViewById(R.id.warning_switch);
+
+        // Set a listener for the switch
+        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Notifications enabled
+                    Toast.makeText(NotificationTypeActivity.this, "Notifications Enabled", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Notifications disabled
+                    Toast.makeText(NotificationTypeActivity.this, "Notifications Disabled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- Group_3/FrontEnd/app/src/main/java/com/example/calamitycall/fragments/BoardsPage.java
+++ Group_3/FrontEnd/app/src/main/java/com/example/calamitycall/fragments/BoardsPage.java
@@ -1,11 +1,9 @@
 package com.example.calamitycall.fragments;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
