6af79d65f9efdd55d1834530ed2c697e3d21ccd4
Sudhan <86077071+Sudhan-Dahake@users.noreply.github.com>
2024-11-10 03:22:20 -0500

    Fixed a typo in argument calling name.

--- Group_3/supabase_db/models/TextToSpeech.py
+++ Group_3/supabase_db/models/TextToSpeech.py
@@ -2,4 +2,4 @@ from . import BasePreference
 
 class TextToSpeechModel(BasePreference):
     def __init__(self):
-        super().__init__(tablename="text_to_speech")
+        super().__init__(tableName="text_to_speech")
