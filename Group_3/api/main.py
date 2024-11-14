c5ec11ac5ebbc5a63e9676767ee1021c45b7af59
Sudhan <86077071+Sudhan-Dahake@users.noreply.github.com>
2024-11-14 07:02:25 -0500

    Setup NWS and EMS endpoints to receive notifications.

--- Group_3/api/main.py
+++ Group_3/api/main.py
@@ -5,10 +5,12 @@ from .routers import Users
 from .routers import Auth
 from .routers import ProtectedJWTVerificiation
 from .routers import Token
+from .routers import Alerts
 
 app = FastAPI()
 
 # including the routers for each module
+app.include_router(Alerts.router, prefix="/alerts", tags=["Alerts"])
 app.include_router(Notifications.router, prefix="/notifications", tags=["Notifications"])
 app.include_router(Preferences.router, prefix="/preferences", tags=["Preferences"])
 app.include_router(Users.router, prefix="/users", tags=["Users"])
--- Group_3/api/routers/Notifications.py
+++ Group_3/api/routers/Notifications.py
@@ -1,24 +1,12 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from .. import NotificationModel
-from ..schemas.Notifications import NotificationCreate, NotificationResponse, NotificationHistoryRequest, NotificationHistoryResponse
+from ..schemas.Notifications import NotificationResponse, NotificationHistoryRequest, NotificationHistoryResponse
 from ...supabase_db import AuthService
 
 
 router = APIRouter()
 
 AuthServiceObj = AuthService()
-
-#Create Notification
-@router.post("/", status_code=status.HTTP_201_CREATED)
-async def CreateNotification(notification: NotificationCreate, username: str = Depends(AuthServiceObj.VerifyJWT)):
-    NotifModel = NotificationModel()
-
-    success = NotifModel.CreateNotification(**notification.model_dump(exclude_none=True))
-
-    if success:
-        return {"Message": "Notification created successfully"}
-
-    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create Notification")
 
 @router.get("/immediate", response_model=NotificationResponse, response_model_exclude_none=True)
 async def GetImmediateNotification(username: str = Depends(AuthServiceObj.VerifyJWT)):
@@ -31,11 +19,11 @@ async def GetImmediateNotification(username: str = Depends(AuthServiceObj.Verify
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No notifications found")
 
-@router.get("/history", response_model=NotificationHistoryResponse, response_model_exclude_none=True)
-async def GetNotificationHistory(timeframe: str = "1 month ago", username: str = Depends(AuthServiceObj.VerifyJWT)):
+@router.post("/history", response_model=NotificationHistoryResponse, response_model_exclude_none=True)
+async def GetNotificationHistory(historyRequest: NotificationHistoryRequest, username: str = Depends(AuthServiceObj.VerifyJWT)):
     NotifModel = NotificationModel()
 
-    NotifDict = NotifModel.GetNotifToDisplayForHistory(timeFrame=timeframe)
+    NotifDict = NotifModel.GetNotifToDisplayForHistory(timeFrame=historyRequest.timeFrame)
 
     if NotifDict:
         NotifList = list(value for key, value in NotifDict.items())
