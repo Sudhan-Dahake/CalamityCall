ff86595d8390e55953aca0567774a1a1bfd2f518
Joel Regi Abraham <Jregiabraham5574@conestogac.on.ca>
2024-11-21 01:05:59 -0500

    updated main

--- Group_10/Api/main.py
+++ Group_10/Api/main.py
@@ -1,20 +1,5 @@
-# from fastapi import FastAPI
-# from .routers import discussion, reports, disaster_reports
-# from routers import discussion, reports, disaster_reports
-
-# app = FastAPI()
-
-
-
-# # Register routers
-# app.include_router(discussion.router, prefix="/api/discussion", tags=["Discussion Board"])
-# app.include_router(reports.router, prefix="/api/reports", tags=["Disaster Reports"])
-# app.include_router(disaster_reports.router, prefix="/api", tags=["Disaster Reports"])
-# # Run the application with `uvicorn main:app --reload`
-
-
 from fastapi import FastAPI
-from .routers import disaster_reports  # Import only what is being used
+from .routers import disaster_reports 
 
 app = FastAPI()
 
